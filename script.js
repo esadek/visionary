7cdea894fbdf607297ee9ee5b0d7c8c65bfecae3
Emil Sadek <esadek@hotmail.com>
2020-12-06 22:09:38 -0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+var results = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+]
+
+var result = document.getElementById("result");
+var button = document.getElementById("btn");
+
+button.onclick = function changeResult() {
+    var index = Math.floor(Math.random() * 20);
+    result.innerHTML = results[index];
+    if (index < 10) {
+        result.style.color = "green";
+    } else if (index < 15) {
+        result.style.color = "yellow";
+    } else {
+        result.style.color = "red";
+    }
+}
